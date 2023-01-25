6e722057a78b3576eedf7576cada7365d53815c6
Shannan Galbraith <s.galbraith08@gmail.com>
2023-01-26 01:29:12 +1100

    55

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,22 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'Dating App';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response) => this.users = response,
+      next: (response) => (this.users = response),
       error: (error) => console.log(error),
-      complete: () => console.log('Req is done.')
-    })
+      complete: () => console.log('Req is done.'),
+    });
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
